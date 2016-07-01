f3df0182545e0ea662bb023a71f7d4ad6d31d5ad
James McCallum <jamesgmccallum@gmail.com>
2016-07-01 16:26:29 +1000

    remove unused hook

--- demo/hooks/before_prepare/plugin_copy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo ----copying files from plugin project----
-pwd
-cp -R ../src/* plugins/io.iclue.backgroundvideo/src/
-cp -R ../www/* plugins/io.iclue.backgroundvideo/www/
-cp ../plugin.xml plugins/io.iclue.backgroundvideo/
-
