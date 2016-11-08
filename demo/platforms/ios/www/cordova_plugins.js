2b7c7c0dbe77b16582f9c3d6b79d0f71b58a81f1
James McCallum <jamesgmccallum@gmail.com>
2016-11-09 08:54:59 +1100

    update install instructions to use git url rather than cordova plugin registry

---
+++ demo/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/io.iclue.backgroundvideo/www/backgroundvideo.js",
+        "id": "io.iclue.backgroundvideo.backgroundvideo",
+        "clobbers": [
+            "cordova.plugins.backgroundvideo"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "io.iclue.backgroundvideo": "0.0.8"
+}
+// BOTTOM OF METADATA
+});
