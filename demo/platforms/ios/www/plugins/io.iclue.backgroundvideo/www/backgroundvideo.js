84b48edcfd6b91ee3e4fd31069bc49993abb5347
James McCallum <jamesgmccallum@gmail.com>
2016-12-04 17:41:46 +1100

    patch

--- demo/platforms/ios/www/plugins/io.iclue.backgroundvideo/www/backgroundvideo.js
+++ demo/platforms/ios/www/plugins/io.iclue.backgroundvideo/www/backgroundvideo.js
@@ -2,7 +2,7 @@ cordova.define("io.iclue.backgroundvideo.backgroundvideo", function(require, exp
 var cordova = require('cordova');
 
 var backgroundvideo = {
-    start : function(filename, camera, successFunction, errorFunction, dontRecordAudio) {
+    start : function(filename, camera, successFunction, errorFunction) {
     	camera = camera || 'back';
         cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [filename, camera, dontRecordAudio]);
     },
--- www/backgroundvideo.js
+++ www/backgroundvideo.js
@@ -2,9 +2,9 @@
 var cordova = require('cordova');
 
 var backgroundvideo = {
-    start : function(filename, camera, successFunction, errorFunction) {
+    start : function(filename, camera, shouldRecordAudio, successFunction, errorFunction) {
     	camera = camera || 'back';
-        cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [filename, camera, dontRecordAudio]);
+        cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [filename, camera, shouldRecordAudio]);
     },
     stop : function(successFunction, errorFunction) {
         cordova.exec(successFunction, errorFunction, "backgroundvideo","stop", []);
