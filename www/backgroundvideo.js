93225d1df895012048039cd42e8a7ef1816d1343
Andrey Fuks <cale148@gmail.com>
2017-04-14 20:35:32 +0500

    updated description

--- www/backgroundvideo.js
+++ www/backgroundvideo.js
@@ -3,10 +3,10 @@ var cordova = require('cordova');
 var backgroundvideo = {
     start : function(fileStorage, filename, camera, quality, successFunction, errorFunction) {
         camera = camera || 'back';
-        cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [fileStorage, filename, camera, quality]);
+        cordova.exec(successFunction, errorFunction, 'backgroundvideo', 'start', [fileStorage, filename, camera, quality]);
     },
     stop : function(successFunction, errorFunction) {
-        cordova.exec(successFunction, errorFunction, "backgroundvideo","stop", []);
+        cordova.exec(successFunction, errorFunction, 'backgroundvideo','stop', []);
     }
 };
 
