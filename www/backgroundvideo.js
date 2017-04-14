3d6d90f431929416d04b5223a606396eb8e91cd0
Andrey Fuks <cale148@gmail.com>
2017-04-14 20:17:49 +0500

    Added another params fileStorage, filename, camera, quality. Set quality to medium in iOS

--- www/backgroundvideo.js
+++ www/backgroundvideo.js
@@ -1,9 +1,9 @@
 var cordova = require('cordova');
 
 var backgroundvideo = {
-    start : function(filename, camera, shouldRecordAudio, successFunction, errorFunction) {
+    start : function(fileStorage, filename, camera, quality, successFunction, errorFunction) {
         camera = camera || 'back';
-        cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [filename, camera, shouldRecordAudio]);
+        cordova.exec(successFunction, errorFunction, "backgroundvideo","start", [fileStorage, filename, camera, quality]);
     },
     stop : function(successFunction, errorFunction) {
         cordova.exec(successFunction, errorFunction, "backgroundvideo","stop", []);
